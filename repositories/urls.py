ee88e4c56c4c0bc773e44fdc5de901aff96a4fb7
João Guilherme <joaaogui@gmail.com>
2019-08-21 21:31:31 -0300

    Displying repos on index after login

--- repositories/urls.py
+++ repositories/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'repositories'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:repository_id>/', views.detail, name='detail'),
+    path('button/', views.button, name='button'),
     path('callback/', views.repos),
+    path('repositories/', views.repos),
 ]
--- repositories/views.py
+++ repositories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 import requests
 from requests_oauthlib import OAuth2Session
@@ -14,17 +14,26 @@ from .models import Repository
 client_id = "181ce723b9ce46e40263"
 token_url = 'https://github.com/login/oauth/access_token'
 client_secret = "f9e0cc40dc6f080c9752a0f79f258683601422a5"
-scope = 'repo'  # we want to fetch user's email
+scope = 'repo'
 
 github = OAuth2Session(client_id, scope=scope)
 
 def index(request):
+    return render(request, 'repositories/index.html')
+
+def button(request):
+    print('chama')
     authorization_base_url = 'https://github.com/login/oauth/authorize'
     authorization_url, state = github.authorization_url(authorization_base_url)
     return redirect(authorization_url)
 
 def detail(request, repository_id):
-    return HttpResponse("You're looking at repository %s." % repository_id)
+    # repository = get_object_or_404(Repository, pk=repository_id)
+
+    return render(request, 'repositories/detail.html', {'repository': repository_id})
+
+# def get_info(token, repository_id):
+#
 
 def repos(request):
     print(request.GET['code'])
@@ -33,19 +42,19 @@ def repos(request):
         'client_secret' :client_secret,
         'code' : request.GET['code']
     }
-    header = 'Accept: application/json'
     r = requests.post('https://github.com/login/oauth/access_token', data=params, headers={"Accept": "application/json"})
     result = r.json()
     token = result.get("access_token", None)
 
     g = Github(token)
 
-    print(g.get_repos())
+    repository_list = []
+    # print(g.get_repo('202746182'))
     for x in g.get_user().get_repos(visibility='all'):
-        print(x)
-    # get_repos = requests.get('https://api.github.com/user', headers={'Authorization': 'token ' + token})
-    # print(get_repos.content)
+        repository_list.append(x)
+        print(x.id)
+        print(" ---------------------------------------- --------------------------------")
 
-    # token = github.fetch_token(token_url, username=client_id, password=client_secret, authorization_response=url)
-    return HttpResponse("YCHama")
+
+    return render(request, 'repositories/index.html', {'repository_list': repository_list})
 
